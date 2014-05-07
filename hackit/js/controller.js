052de6f42a6f06d6b2e86f4ef28c1eb23792c35d
cokepluscarbon <93948886@qq.com>
2014-05-07 18:01:40 +0800

    试水成功

--- hackit/js/controller.js
+++ hackit/js/controller.js
@@ -2,3 +2,29 @@ $('.nav a').click(function(e) {
 	e.preventDefault()
 	$(this).tab('show')
 })
+
+$.post('http://localhost:8080/hackit/encode/md5', {
+	text : 'this is test text!这是测试文档！',
+	encoding : 'UTF-8'
+}, function(data) {
+	console.log(data);
+}, 'json')
+
+$('#plaintext-encode-trigger').click(function(e) {
+	var text = $('#plainText').val();
+
+	console.log(text);
+	$('#text').val(text);
+
+	var $form = $('#encode-plaintext-form');
+	var url = $form.attr('action');
+	var params = $form.serialize();
+
+	$.post(url, params, function(data) {
+		$('#hexText').val(data.hexText);
+		$('#binaryText').val(data.binaryText);
+		$('#urlText').val(data.urlText);
+		$('#base64Text').val(data.base64Text);
+		$('#base64UrlSafeText').val(data.base64UrlSafeText);
+	}, 'json');
+})
