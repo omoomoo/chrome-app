37e1da04308d843bb23740b9d6746b8fcabc2028
cokepluscarbon <93948886@qq.com>
2014-05-08 20:39:37 +0800

    add common lang

--- hackit-server/src/main/java/com/github/cokepluscarbon/EnDecodeController.java
+++ hackit-server/src/main/java/com/github/cokepluscarbon/EnDecodeController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.tags.HtmlEscapeTag;
 
 @Controller
 public class EnDecodeController {
