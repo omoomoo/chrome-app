a732ec1700119de67c2c62db3a3425f82c70cc05
cokepluscarbon <93948886@qq.com>
2014-05-12 13:36:38 +0800

    fix

--- hackit/background.js
+++ hackit/background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
-      'width': 1440,
-      'height': 900
+      'width': 1280,
+      'height': 800
     }
   });
 });
