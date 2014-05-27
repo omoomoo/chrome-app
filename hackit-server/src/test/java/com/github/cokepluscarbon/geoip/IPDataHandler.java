82f2727ceb13e67c4b9f32bcf1893a1054434596
cokepluscarbon <93948886@qq.com>
2014-05-27 19:27:01 +0800

    FIX

--- hackit-server/src/test/java/com/github/cokepluscarbon/geoip/IPDataHandler.java
+++ hackit-server/src/test/java/com/github/cokepluscarbon/geoip/IPDataHandler.java
@@ -33,6 +33,8 @@ import org.apache.commons.lang.StringUtils;
 public class IPDataHandler {
 	public static void main(String[] args) {
 		System.out.println(IPDataHandler.findGeography("221.4.213.1").split("\t").length);
+		System.out.println(IPDataHandler.fileLength);
+		System.out.println(IPDataHandler.dataLength);
 	}
 
 	private static String IP_DATA_PATH = "C://17monipdb.dat";
